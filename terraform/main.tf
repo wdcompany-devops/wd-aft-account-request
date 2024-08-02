e34ab7bad04f8b0b65a8ddfb70f75b4bd927fc5d
Laura Oliveira <laura.oliveira@iteris.com.br>
2024-08-02 11:17:15 -0300

    Removing the account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-module "wd-pixar" {
-  source = "./modules/aft-account-request"
+# module "wd-pixar" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "wd-pixar-271828@gmail.com"
-    AccountName               = "wd-pixar-dev"
-    ManagedOrganizationalUnit = "FullSteamPay (ou-bukh-eo7srkvx)"
-    SSOUserEmail              = "laura.oliveira@iteris.com.br"
-    SSOUserFirstName          = "Laura"
-    SSOUserLastName           = "Oliveira"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "wd-pixar-271828@gmail.com"
+#     AccountName               = "wd-pixar-dev"
+#     ManagedOrganizationalUnit = "FullSteamPay (ou-bukh-eo7srkvx)"
+#     SSOUserEmail              = "laura.oliveira@iteris.com.br"
+#     SSOUserFirstName          = "Laura"
+#     SSOUserLastName           = "Oliveira"
+# }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "Laura Oliveira"
-    change_reason       = "Updating settings for new business unit"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "Laura Oliveira"
+#     change_reason       = "Updating settings for new business unit"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "wd-pixar"
-}
+#   account_customizations_name = "wd-pixar"
+# }
